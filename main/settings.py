6bc73e9388b9d27f99073d175fcd4ce1a1be404a
Suraj Kumar <suraj.iitb.in@gmail.com>
2021-02-14 19:14:42 +0530

    Postgres DB in Docker Service

--- main/settings.py
+++ main/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elearning_academy',
+        'USER': 'bodhitree',
+        'PASSWORD': 'bodhitree123',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 

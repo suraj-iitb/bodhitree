56194ef2d52fd7a01fd86cfc232b2be5d29d6fbf
Poornima <sridhar.poornima18@gmail.com>
2021-03-11 17:36:07 +0530

    added CourseHistory table

--- course/models.py
+++ course/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 course_types = (
-    ('O', "Open"),
-    ('M', "Moderated")
+    ('O', 'Open'),
+    ('M', 'Moderated')
 )
+
+user_roles = (
+    ('I', 'Instructor'),
+    ('T', 'Teaching Assistant'),
+    ('S', 'Student')
+)
+
+status_type = (
+    ('E', 'Enrolled'),
+    ('U', 'Unenrolled'),
+    ('P', 'Pending')
+)
+
 class Course(models.Model):
-    owner_id = models.IntegerField() # We will replace this with foreign key in future
+    owner = models.IntegerField() # We will replace this with foreign key in future
     code = models.CharField(max_length=8,blank=True)
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
@@ -17,3 +30,11 @@ class Course(models.Model):
     modified_on = models.DateTimeField(auto_now=True)
     chapters_sequence = ArrayField(models.IntegerField(), null=True, blank=True)
     
+class CourseHistory(models.Model):
+    user = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, choices=user_roles, default='S')
+    status = models.CharField(max_length=1, choices=status_type, default='P')
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+

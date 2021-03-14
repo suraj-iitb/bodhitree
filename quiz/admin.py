74596a75880f0959383f9cb298815ee3befb4f50
Poornima <sridhar.poornima18@gmail.com>
2021-03-14 21:50:32 +0530

    Added tables of quiz tables under model and admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import (Quiz, QuestionModule, Question, QuestionHistory, SingleCorrectQuestion, SingleCorrectQuestionHistory, MultipleCorrectQuestion,
+MulitpleCorrectQuestionHistory, FixedAnswerQuestion, FixedCorrectQuestionHistory, DescriptiveQuestion, DescriptiveQuestionHistory)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'chapter', 'section')
+
+class QuestionModuleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'quiz')
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quiz_module' ,'question_description', 'answer_description')
+
+class QuestionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'user', 'no_of_times_attempted')
+
+class SingleCorrectQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'options', 'correct_option')
+
+class SingleCorrectQuestionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_history', 'option_selected')
+
+class MultipleCorrectQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'options', 'correct_option')
+
+class MulitpleCorrectQuestionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_history', 'option_selected')
+
+class FixedAnswerQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer')
+
+class FixedCorrectQuestionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_history', 'answer_submitted')
+
+class DescriptiveQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer')
+
+class DescriptiveQuestionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_history', 'answer_submitted')
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(QuestionModule, QuestionModuleAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(QuestionHistory, QuestionHistoryAdmin)
+admin.site.register(SingleCorrectQuestion, SingleCorrectQuestionAdmin)
+admin.site.register(SingleCorrectQuestionHistory, SingleCorrectQuestionHistoryAdmin)
+admin.site.register(MultipleCorrectQuestion, MultipleCorrectQuestionAdmin)
+admin.site.register(MulitpleCorrectQuestionHistory, MulitpleCorrectQuestionHistoryAdmin)
+admin.site.register(FixedAnswerQuestion, FixedAnswerQuestionAdmin)
+admin.site.register(FixedCorrectQuestionHistory, FixedCorrectQuestionHistoryAdmin)
+admin.site.register(DescriptiveQuestion, DescriptiveQuestionAdmin)
+admin.site.register(DescriptiveQuestionHistory, DescriptiveQuestionHistoryAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from course.models import (
+    Chapter, Section
+)
+from django.conf import settings
 
-# Create your models here.
+class Quiz(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True, blank=True)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE,  null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    question_module_sequence = ArrayField(models.IntegerField(), null=True, blank=True)    
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on= models.DateTimeField(auto_now=True)
+
+class QuestionModule(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    questions_sequence = ArrayField(models.IntegerField(), null=True, blank=True)    
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on= models.DateTimeField(auto_now=True)
+
+class Question(models.Model):
+    quiz_module = models.ForeignKey(QuestionModule, on_delete=models.CASCADE)
+    question_description = models.TextField()                             
+    answer_description = models.TextField()                               
+    hint = models.TextField(blank=True)
+    max_no_of_attempts = models.IntegerField(blank=True, null=True)
+    marks = models.IntegerField(default=0)
+    gradable = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on= models.DateTimeField(auto_now=True)
+
+class QuestionHistory(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    no_of_times_attempted = models.IntegerField()
+    marks_obtained = models.IntegerField(null=True, blank=True)
+    hint_taken = models.BooleanField(default=False)
+
+class SingleCorrectQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    options = ArrayField(models.TextField())    
+    correct_option = models.IntegerField()
+
+class SingleCorrectQuestionHistory(models.Model):
+    question_history = models.ForeignKey(QuestionHistory, on_delete=models.CASCADE)
+    option_selected = models.IntegerField()
+
+class MultipleCorrectQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    options = ArrayField(models.TextField())    
+    correct_option = ArrayField(models.IntegerField())
+
+class MulitpleCorrectQuestionHistory(models.Model):
+    question_history = models.ForeignKey(QuestionHistory, on_delete=models.CASCADE)
+    option_selected = ArrayField(models.IntegerField())
+
+class FixedAnswerQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+
+class FixedCorrectQuestionHistory(models.Model):
+    question_history = models.ForeignKey(QuestionHistory, on_delete=models.CASCADE)
+    answer_submitted = models.TextField()
+
+class DescriptiveQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+
+class DescriptiveQuestionHistory(models.Model):
+    question_history = models.ForeignKey(QuestionHistory, on_delete=models.CASCADE)
+    answer_submitted = models.TextField()
+

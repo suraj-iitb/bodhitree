5367d316f5472c63debd2377f0c757ffe8f887fd
Poornima <sridhar.poornima18@gmail.com>
2021-03-15 12:20:29 +0530

    Added tables of crib , discussion_forum under models and admin

--- cribs/admin.py
+++ cribs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from cribs.models import (Crib, CribReply)
+
+
+class CribAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'created_by', 'title')
+
+
+class CribReplyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'crib', 'user')
+
+admin.site.register(Crib, CribAdmin)
+admin.site.register(CribReply, CribReplyAdmin)
--- discussion_forum/admin.py
+++ discussion_forum/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from discussion_forum.models import (DiscussionForum, Content, DiscussionThread, DiscussionComment, DiscussionReply, Tag, DiscussionThreadTag)
+
+
+class DiscussionForumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course')
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'content')
+
+class DiscussionThreadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'discussion_forum')
+
+
+class DiscussionCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'discussion_thread')
+
+class DiscussionReplyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'discussion_comment')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'discussion_forum')
+
+
+class DiscussionThreadTagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tag', 'discussion_thread')
+
+
+admin.site.register(DiscussionForum, DiscussionForumAdmin)
+admin.site.register(Content, ContentAdmin)
+admin.site.register(DiscussionThread, DiscussionThreadAdmin)
+admin.site.register(DiscussionComment, DiscussionCommentAdmin)
+admin.site.register(DiscussionReply, DiscussionReplyAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(DiscussionThreadTag, DiscussionThreadTagAdmin)
+

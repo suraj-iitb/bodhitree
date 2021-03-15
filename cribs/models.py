7d566c4e720d29bb777df81ff18c329d6ec2038e
Poornima <sridhar.poornima18@gmail.com>
2021-03-15 12:20:29 +0530

    Added tables of crib , discussion_forum under models and admin

--- cribs/models.py
+++ cribs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from course.models import Course
 
-# Create your models here.
+class Crib(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
+    title = models.CharField(max_length=75)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on= models.DateTimeField(auto_now=True)
+
+class CribReply(models.Model):
+    crib = models.ForeignKey(Crib, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
--- discussion_forum/models.py
+++ discussion_forum/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
+from course.models import Course
 
-# Create your models here.
+user_roles = (
+    ('I', 'Instructor'),
+    ('T', 'Teaching Assistant'),
+    ('S', 'Student')
+)
+
+class DiscussionForum(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    anonymous_to_instructor = models.BooleanField(default=False)
+    send_email_to_all = models.BooleanField(default=False)
+
+class Content(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_category = models.CharField(max_length=1, choices=user_roles, default='S')
+    content = models.TextField()
+    pinned = models.BooleanField(default=False)
+    anonymous_to_student = models.BooleanField(default=False)
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+class DiscussionThread(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    discussion_forum = models.ForeignKey(DiscussionForum, on_delete=models.CASCADE)
+    mark_as_important = models.BooleanField(default=False)
+
+class DiscussionComment(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    discussion_thread = models.ForeignKey(DiscussionThread, on_delete=models.CASCADE)
+
+class DiscussionReply(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    discussion_comment = models.ForeignKey(DiscussionComment, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    discussion_forum = models.ForeignKey(DiscussionForum, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    tag_name = models.TextField()   #check
+
+class DiscussionThreadTag(models.Model):
+    discussion_thread = models.ForeignKey(DiscussionThread, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+

9272e0759831da9e4d9c6bcff93a713872189113
suraj <suraj.iitb.in@gmail.com>
2021-03-20 12:14:09 +0530

    subjective assign review done

--- subjective_assignments/admin.py
+++ subjective_assignments/admin.py
@@ -4,12 +4,15 @@ from subjective_assignments.models import (SubjectiveAssignment, SubjectiveAssig
 
 class SubjectiveAssignmentAdmin(admin.ModelAdmin):
     list_display = ('id','assignment_id', 'team_size')
+    search_fields = ('assignment__name',)
 
 class SubjectiveAssignmentHistoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'assignment_history', 'submitted_by', 'subjective_assignment_team')
+    search_fields = ('submitted_by__email',)
 
 class SubjectiveAssignmentTeamAdmin(admin.ModelAdmin):
-    list_display = ('id'. 'user', 'subjective_assignment')
+    list_display = ('id', 'user', 'subjective_assignment')
+    search_fields = ('user__email',)
 
 admin.site.register(SubjectiveAssignment, SubjectiveAssignmentAdmin)
 admin.site.register(SubjectiveAssignmentHistory, SubjectiveAssignmentHistoryAdmin)
--- subjective_assignments/models.py
+++ subjective_assignments/models.py
@@ -2,6 +2,7 @@ import os
 from django.db import models
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
+from programming_assignments.models import Assignment, AssignmentHistory
 
 def assignment_file_upload_path(instance, filename):
         course = instance.assignment.course
@@ -12,18 +13,23 @@ def assignment_file_upload_path(instance, filename):
         return os.path.join(assignment_path + '/question_files/' + filename) 
 
 class SubjectiveAssignment(models.Model):
-    assignment = models.IntegerField()  #link
+    assignment = models.OneToOneField(Assignment,on_delete=models.CASCADE)  
     team_size = models.IntegerField()
     question_file = models.FileField(upload_to=assignment_file_upload_path)
     helper_file = models.FileField(upload_to=assignment_file_upload_path)
     files_to_be_submitted = ArrayField(models.IntegerField(), null=True, blank=True)
 
+    def __str__(self):
+        return self.assignment.name 
+
+class SubjectiveAssignmentTeam(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subjective_assignment = models.ForeignKey(SubjectiveAssignment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subjective_assignment.assignment.name + ':' + self.user.email
 class SubjectiveAssignmentHistory(models.Model):
-    assignment_history = models.IntegerField()  #link
+    assignment_history = models.OneToOneField(AssignmentHistory, on_delete=models.CASCADE)  
     submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     marks_obtained = models.FloatField()
     subjective_assignment_team = models.ForeignKey(SubjectiveAssignmentTeam, on_delete=models.CASCADE)
-
-class SubjectiveAssignmentTeam(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    subjective_assignment = models.ForeignKey(SubjectiveAssignment, on_delete=models.CASCADE)

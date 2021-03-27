40174f7ab7cc38e84e629e73667735d51267d7bb
suraj <suraj.iitb.in@gmail.com>
2021-03-27 11:52:02 +0530

    Video models done

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Video, VideoHistory, SectionMarker, QuizMarker)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chapter', 'section', 'title', 'description',
+                    'video_file', 'doc_file', 'in_video_quiz_file',
+                    'video_duration', 'uploaded_on', 'modified_on')
+    search_fields = ('title',)
+
+
+class VideoHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video', 'user', 'video_watched_duration')
+    search_fields = ('video__title', 'user__email')
+
+
+class SectionMarkerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video', 'time', 'title', 'created_on', 'modified_on')
+    search_fields = ('video__title', 'title')
+
+
+class QuizMarkerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video', 'time', 'title', 'quiz', 'created_on',
+                    'modified_on')
+    search_fields = ('video__title', 'title')
+
+
+admin.site.register(Video, VideoAdmin)
+admin.site.register(VideoHistory, VideoHistoryAdmin)
+admin.site.register(SectionMarker, SectionMarkerAdmin)
+admin.site.register(QuizMarker, QuizMarkerAdmin)
--- video/models.py
+++ video/models.py
@@ -1,49 +1,37 @@
+# In-built imports
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+from datetime import timedelta
 import os
-from django.db import models
+# Other imports
 from course.models import (Chapter, Section)
 from quiz.models import Quiz
-from django.conf import settings
-from datetime import datetime, timedelta
-
-#can be optimized to 1 function later
 
 
 def video_upload_path(instance, filename):
-    course = instance.chapter.course
+    course = instance.chapter.course if instance.chapter else instance.section.chapter.course
     return os.path.join(
-        str(course.id) + '-' + course.title + '/videos/' + filename)
+        str(course.id) + '.' + course.title, 'video_files', filename)
 
 
 def video_doc_upload_path(instance, filename):
-    course = instance.chapter.course
+    course = instance.chapter.course if instance.chapter else instance.section.chapter.course
     return os.path.join(
-        str(course.id) + '-' + course.title + '/video_doc_files/' + filename)
+        str(course.id) + '.' + course.title, 'video_doc_files', filename)
 
 
 def in_video_quiz_upload_path(instance, filename):
-    course = instance.chapter.course
+    course = instance.chapter.course if instance.chapter else instance.section.chapter.course
     return os.path.join(
-        str(course.id) + '-' + course.title + '/in_video_quiz_files/' +
-        filename)
-
-
-marker_type = (
-    ('S', 'Section marker'),
-    ('Q', 'Quiz marker'),
-)
-
-#section, chapter can be made null and they can be together not null
+        str(course.id) + '.' + course.title, 'in_video_quiz_files', filename)
 
 
 class Video(models.Model):
-    section = models.ForeignKey(Section,
+    chapter = models.ForeignKey(Chapter,
                                 on_delete=models.CASCADE,
                                 blank=True,
                                 null=True)
-    chapter = models.ForeignKey(Chapter,
+    section = models.ForeignKey(Section,
                                 on_delete=models.CASCADE,
                                 blank=True,
                                 null=True)
@@ -51,36 +39,53 @@ class Video(models.Model):
     description = models.TextField(blank=True)
     video_file = models.FileField(upload_to=video_upload_path)
     doc_file = models.FileField(upload_to=video_doc_upload_path,
-                                null=True,
-                                blank=True)
-    invideo_quiz_file = models.FileField(upload_to=in_video_quiz_upload_path,
-                                         null=True,
-                                         blank=True)
+                                blank=True,
+                                null=True)
+    in_video_quiz_file = models.FileField(upload_to=in_video_quiz_upload_path,
+                                          blank=True,
+                                          null=True)
     video_duration = models.DurationField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
 
 class VideoHistory(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    video_watched_duration = models.DurationField(default=timedelta(seconds=0))
+    video_watched_duration = models.DurationField(default=timedelta())
+
+
+marker_type = (
+    ('S', 'Section Marker'),
+    ('Q', 'Quiz Marker'),
+)
 
 
 class Marker(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     time = models.DurationField()
-    marker_type = models.CharField(max_length=1, choices=marker_type)
+    title = models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
-
-class SectionMarker(models.Model):
-    marker = models.OneToOneField(Marker, on_delete=models.CASCADE)
-    title = models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
+    class Meta:
+        abstract = True
 
 
-class QuizMarker(models.Model):
-    marker = models.OneToOneField(Marker, on_delete=models.CASCADE)
-    title = models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+class SectionMarker(Marker):
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['video', 'time'],
+                                    name='unique_section_marker')
+        ]
+
+
+class QuizMarker(Marker):
+    quiz = models.OneToOneField(Quiz, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['video', 'time'],
+                                    name='unique_quiz_marker')
+        ]

473bf3d5dd0ff54e2966905b474c15bdcdc9a3df
suraj <suraj.iitb.in@gmail.com>
2021-03-28 20:40:16 +0530

    crib models and admin

--- cribs/models.py
+++ cribs/models.py
@@ -4,20 +4,32 @@ from django.db import models
 from course.models import Course
 
 
+CRIB_STATUS = (
+    ("R", "Resolved"),
+    ("U", "Unresolved"),
+)
+
+
 class Crib(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    assigned_to = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="crib_creator"
     )
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
     description = models.TextField(blank=True)
+    status = models.CharField(max_length=1, choices=CRIB_STATUS, default="U")
     created_on = models.DateTimeField(auto_now_add=True)
-    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 class CribReply(models.Model):
     crib = models.ForeignKey(Crib, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description

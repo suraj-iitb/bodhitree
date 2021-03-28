4fee02ef76522cc2136d53fe352aceda7d1cc6ac
suraj <suraj.iitb.in@gmail.com>
2021-03-28 20:02:46 +0530

    Announcements models and admin done

--- announcements/models.py
+++ announcements/models.py
@@ -9,3 +9,6 @@ class Announcement(models.Model):
     is_pinned = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body

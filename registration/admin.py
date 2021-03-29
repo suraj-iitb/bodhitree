b40935e008518b49a9d50b9472c8e81f02a87aa6
suraj <suraj.iitb.in@gmail.com>
2021-03-29 21:36:42 +0530

    Registration Models Done

--- registration/admin.py
+++ registration/admin.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
 from .forms import UserAdminChangeForm, UserAdminCreationForm
-from .models import PlanType, Subscription, SubscriptionHistory, User
+from .models import PlanType, Subscription, SubscriptionHistory
 
 
 class UserAdmin(BaseUserAdmin):
@@ -14,55 +15,105 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ("email", "admin")
-    list_filter = ("admin",)
+    list_display = (
+        "id",
+        "email",
+        "full_name",
+        "is_active",
+        "is_admin",
+        "date_joined",
+        "last_login",
+    )
+    list_filter = (
+        "is_admin",
+        "is_active",
+    )
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
         ("Personal info", {"fields": ("full_name",)}),
-        ("Permissions", {"fields": ("admin",)}),
-        ("Important dates", {"fields": ("last_login",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_admin",
+                )
+            },
+        ),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
-        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
     )
-    search_fields = ("email",)
-    ordering = ("email",)
+    search_fields = (
+        "email",
+        "full_name",
+    )
     filter_horizontal = ()
 
 
 class PlanTypeAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = (
+        "id",
+        "name",
+    )
     search_fields = ("name",)
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
+        "id",
         "plan_type",
         "no_of_courses",
         "no_of_students_per_course",
         "prog_assign_enabled",
-        "subjective_lab_enabled",
+        "subjective_assign_enabled",
         "email_enabled",
         "per_video_limit",
         "per_video_limit_unit",
         "total_video_limit",
         "total_video_limit_unit",
-        "subjective_lab_submission_size_per_student",
-        "subjective_lab_submission_size_per_student_unit",
-    ]
+        "subjective_assign_submission_size_per_student",
+        "subjective_assign_submission_size_per_student_unit",
+    )
     search_fields = ("plan_type__name",)
 
 
 class SubscriptionHistoryAdmin(admin.ModelAdmin):
-    list_display = ["user", "subscription", "start_date", "duration"]
-    search_fields = ["user__email"]
+    list_display = (
+        "id",
+        "user",
+        "subscription",
+        "start_date",
+        "duration",
+        "purchased_on",
+        "modified_on",
+    )
+    search_fields = ("user__email",)
 
 
+admin.site.register(settings.AUTH_USER_MODEL, UserAdmin)
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
 admin.site.register(PlanType, PlanTypeAdmin)
 admin.site.register(SubscriptionHistory, SubscriptionHistoryAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
-admin.site.register(User, UserAdmin)
-# Remove Group Model from admin. We're not using it.
-admin.site.unregister(Group)
--- registration/forms.py
+++ registration/forms.py
@@ -1,31 +1,6 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
-from .models import User
-
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ("email",)
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        qs = User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("email is taken")
-        return email
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
 
 
 class UserAdminCreationForm(forms.ModelForm):
@@ -40,7 +15,7 @@ class UserAdminCreationForm(forms.ModelForm):
     )
 
     class Meta:
-        model = User
+        model = settings.AUTH_USER_MODEL
         fields = (
             "full_name",
             "email",
@@ -78,8 +53,14 @@ class UserAdminChangeForm(forms.ModelForm):
     )
 
     class Meta:
-        model = User
-        fields = ("full_name", "email", "password", "active", "admin")
+        model = settings.AUTH_USER_MODEL
+        fields = (
+            "full_name",
+            "email",
+            "password",
+            "is_active",
+            "is_admin",
+        )
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.

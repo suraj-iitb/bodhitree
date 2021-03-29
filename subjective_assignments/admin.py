84dff133ac7317ae00be50a16be5d834a5965b27
suraj <suraj.iitb.in@gmail.com>
2021-03-29 14:19:44 +0530

    subjective submissiions models

--- subjective_assignments/admin.py
+++ subjective_assignments/admin.py
@@ -8,23 +8,44 @@ from subjective_assignments.models import (
 
 
 class SubjectiveAssignmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "assignment_id", "team_size")
-    search_fields = ("assignment__name",)
+    list_display = (
+        "id",
+        "course",
+        "name",
+        "description",
+        "is_published",
+        "start_date",
+        "end_date",
+        "extended_date",
+        "created_on",
+        "modified_on",
+        "team_size",
+        "question_file",
+        "helper_file",
+        "files_to_be_submitted",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
 
 
 class SubjectiveAssignmentHistoryAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "assignment_history",
-        "submitted_by",
+        "user",
+        "instructor_feedback",
+        "created_on",
+        "modified_on",
+        "marks_obtained",
         "subjective_assignment_team",
     )
-    search_fields = ("submitted_by__email",)
+    search_fields = ("user__email",)
 
 
 class SubjectiveAssignmentTeamAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "subjective_assignment")
-    search_fields = ("user__email",)
+    list_display = ("id", "user_ids", "subjective_assignment")
+    search_fields = ("user_ids",)
 
 
 admin.site.register(SubjectiveAssignment, SubjectiveAssignmentAdmin)

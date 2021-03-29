80c56783f00a8d3ec5fdd9a9ba1bad3557ba1898
suraj <suraj.iitb.in@gmail.com>
2021-03-29 08:04:38 +0530

    Leaderboard models

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,22 +1,35 @@
+from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 from course.models import Course
 
 
-class GradesHeader(models.Model):
+class MarksHeader(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    sheet_name = models.TextField()
-    header_list = ArrayField(models.TextField(), null=True, blank=True)
+    sheet_name = models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
+    header_list = ArrayField(models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH))
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["course", "sheet_name"], name="unique_sheet"
+            )
+        ]
 
     def __str__(self):
         return self.sheet_name
 
 
-class GradesBody(models.Model):
-    grades_header = models.ForeignKey(GradesHeader, on_delete=models.CASCADE)
-    student_marks = ArrayField(models.IntegerField(), null=True, blank=True)
+class MarksBody(models.Model):
+    marks_header = models.ForeignKey(MarksHeader, on_delete=models.CASCADE)
+    student_marks = ArrayField(
+        models.CharField(max_length=settings.MAX_CHARFIELD_LENGTH)
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.student_marks

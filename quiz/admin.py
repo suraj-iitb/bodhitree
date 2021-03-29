dec9718742ea911bf83050e19d46418acedfafcc
suraj <suraj.iitb.in@gmail.com>
2021-03-29 11:49:12 +0530

    Quiz models and admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -7,8 +7,6 @@ from quiz.models import (
     FixedCorrectQuestionHistory,
     MulitpleCorrectQuestionHistory,
     MultipleCorrectQuestion,
-    Question,
-    QuestionHistory,
     QuestionModule,
     Quiz,
     SingleCorrectQuestion,
@@ -17,81 +15,165 @@ from quiz.models import (
 
 
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "chapter", "section")
-    search_fields = ("title", "chapter__name", "section__name")
+    list_display = (
+        "id",
+        "title",
+        "chapter",
+        "section",
+        "description",
+        "question_module_sequence",
+        "created_on",
+        "modified_on",
+    )
+    search_fields = (
+        "title",
+        "description",
+    )
 
 
 class QuestionModuleAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "quiz")
-    search_fields = ("title", "quiz__title")
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "quiz_module", "question_description", "answer_description")
-    search_fields = ("question_description", "quiz_module__title")
-
-
-class QuestionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "user", "no_of_times_attempted")
-    search_fields = ("question__question_description", "user__email")
+    list_display = (
+        "id",
+        "title",
+        "quiz",
+        "description",
+        "questions_sequence",
+        "created_on",
+        "modified_on",
+    )
+    search_fields = ("title", "description")
 
 
 class SingleCorrectQuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "options", "correct_option")
-    search_fields = ("question__question_description",)
+    list_display = (
+        "id",
+        "question_module",
+        "question_description",
+        "answer_description",
+        "hint",
+        "max_no_of_attempts",
+        "marks",
+        "gradable",
+        "is_published",
+        "created_on",
+        "modified_on" "options",
+        "correct_option",
+    )
+    search_fields = ("question_description",)
 
 
 class SingleCorrectQuestionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "question_history", "option_selected")
-    search_fields = (
-        "question_history__question__question_description",
-        "question_history__user__email",
+    list_display = (
+        "id",
+        "question",
+        "user",
+        "no_of_times_attempted",
+        "marks_obtained",
+        "hint_taken",
+        "created_on",
+        "modified_on" "option_selected",
     )
+    search_fields = ("question_history__user__email",)
 
 
 class MultipleCorrectQuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "options", "correct_option")
-    search_fields = ("question__question_description",)
+    list_display = (
+        "id",
+        "question_module",
+        "question_description",
+        "answer_description",
+        "hint",
+        "max_no_of_attempts",
+        "marks",
+        "gradable",
+        "is_published",
+        "created_on",
+        "modified_on" "options",
+        "correct_option",
+    )
+    search_fields = ("question_description",)
 
 
 class MulitpleCorrectQuestionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "question_history", "option_selected")
-    search_fields = (
-        "question_history__question__question_description",
-        "question_history__user__email",
+    list_display = (
+        "id",
+        "question",
+        "user",
+        "no_of_times_attempted",
+        "marks_obtained",
+        "hint_taken",
+        "created_on",
+        "modified_on" "option_selected",
     )
+    search_fields = ("question_history__user__email",)
 
 
 class FixedAnswerQuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "answer")
-    search_fields = ("question__question_description",)
+    list_display = (
+        "id",
+        "question_module",
+        "question_description",
+        "answer_description",
+        "hint",
+        "max_no_of_attempts",
+        "marks",
+        "gradable",
+        "is_published",
+        "created_on",
+        "modified_on" "options",
+        "correct_option",
+    )
+    search_fields = ("question_description",)
 
 
 class FixedCorrectQuestionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "question_history", "answer_submitted")
-    search_fields = (
-        "question_history__question__question_description",
-        "question_history__user__email",
+    list_display = (
+        "id",
+        "question",
+        "user",
+        "no_of_times_attempted",
+        "marks_obtained",
+        "hint_taken",
+        "created_on",
+        "modified_on" "option_selected",
     )
+    search_fields = ("question_history__user__email",)
 
 
 class DescriptiveQuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "answer")
-    search_fields = ("question__question_description",)
+    list_display = (
+        "id",
+        "question_module",
+        "question_description",
+        "answer_description",
+        "hint",
+        "max_no_of_attempts",
+        "marks",
+        "gradable",
+        "is_published",
+        "created_on",
+        "modified_on" "options",
+        "correct_option",
+    )
+    search_fields = ("question_description",)
 
 
 class DescriptiveQuestionHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "question_history", "answer_submitted")
-    search_fields = (
-        "question_history__question__question_description",
-        "question_history__user__email",
+    list_display = (
+        "id",
+        "question",
+        "user",
+        "no_of_times_attempted",
+        "marks_obtained",
+        "hint_taken",
+        "created_on",
+        "modified_on" "option_selected",
     )
+    search_fields = ("question_history__user__email",)
 
 
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(QuestionModule, QuestionModuleAdmin)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(QuestionHistory, QuestionHistoryAdmin)
 admin.site.register(SingleCorrectQuestion, SingleCorrectQuestionAdmin)
 admin.site.register(SingleCorrectQuestionHistory, SingleCorrectQuestionHistoryAdmin)
 admin.site.register(MultipleCorrectQuestion, MultipleCorrectQuestionAdmin)

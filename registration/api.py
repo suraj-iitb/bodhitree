8234304e180089ac66bea526fc4ec557eb83088b
suraj <suraj.iitb.in@gmail.com>
2021-04-03 12:19:57 +0530

    logout functionality with blacklist tokens

--- registration/api.py
+++ registration/api.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import User
 from .serializers import UserSerializer
@@ -7,3 +10,16 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(
+        methods=["post"],
+        detail=False,
+    )
+    def logout(self, request, pk=None):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(e, status=status.HTTP_400_BAD_REQUEST)

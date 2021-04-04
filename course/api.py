2195b456769286543156bdf1a21a1323267e75a7
Poornima <sridhar.poornima18@gmail.com>
2021-04-04 17:08:11 +0530

    permissions for course api

--- course/api.py
+++ course/api.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+
+from utils.drf_utils import IsInstructorOrTAOrReadOnly
 
 from .models import Course
 from .serializers import CourseSerializer
@@ -8,7 +9,7 @@ from .serializers import CourseSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsInstructorOrTAOrReadOnly,)
     filterset_fields = (
         "owner__email",
         "code",
--- utils/drf_utils.py
+++ utils/drf_utils.py
@@ -1,4 +1,8 @@
+from django.db.models import Q
+from rest_framework import permissions
 from rest_framework.pagination import PageNumberPagination
+
+from course.models import CourseHistory
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -9,3 +13,39 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 1000
+
+
+class IsInstructorOrTAOrReadOnly(permissions.BasePermission):
+    """
+    Allows complete permission to instructor/ta and access to others
+    """
+
+    def has_permission(self, request, view):
+
+        """
+        Returns whether user has permission on this model or not
+        """
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.is_authenticated:
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Returns whether user has permission on this object or not
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.is_authenticated:
+            course_histories = CourseHistory.objects.filter(
+                Q(course=obj) & (Q(role="I") | Q(role="T"))
+            )
+            for course_history in course_histories:
+                if course_history.user == request.user:
+                    return True
+            return False

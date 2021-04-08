3ce6b1ddcc828158575704f1d60d211f03cebf1d
Poornima <sridhar.poornima18@gmail.com>
2021-04-08 12:27:12 +0530

    Added IsInstructorOrTAOrStudent permission

--- utils/drf_utils.py
+++ utils/drf_utils.py
@@ -2,7 +2,16 @@ from django.db.models import Q
 from rest_framework import permissions
 from rest_framework.pagination import PageNumberPagination
 
-from course.models import Announcement, Chapter, CourseHistory, Page, Schedule, Section
+from course.models import (
+    Announcement,
+    Chapter,
+    CourseHistory,
+    Notification,
+    Page,
+    Schedule,
+    Section,
+)
+from cribs.models import Crib
 from document.models import Document
 from quiz.models import (
     DescriptiveQuestion,
@@ -35,7 +44,7 @@ class IsInstructorOrTA(permissions.BasePermission):
                     QuestionModule, SingleCorrectQuestion, MultipleCorrectQuestion,
                     FixedAnswerQuestion, DescriptiveQuestion,
                     Chapter, Schedule, Page, Announcement,
-                    Section
+                    Section, Notification
     """
 
     def has_permission(self, request, view):
@@ -54,6 +63,8 @@ class IsInstructorOrTA(permissions.BasePermission):
         if request.user.is_authenticated:
             if request.method in permissions.SAFE_METHODS:
                 return True
+
+            # find course using obj instance
             if type(obj) in (Video, Document, Quiz):
                 course = (
                     obj.chapter.course if obj.chapter else obj.section.chapter.course
@@ -81,10 +92,11 @@ class IsInstructorOrTA(permissions.BasePermission):
                     if obj.question_module.quiz.chapter
                     else obj.question_module.quiz.section.chapter.course
                 )
-            elif type(obj) in (Chapter, Schedule, Announcement, Page):
+            elif type(obj) in (Chapter, Schedule, Announcement, Page, Notification):
                 course = obj.course
             elif type(obj) == Section:
                 course = obj.chapter.course
+
             course_histories = CourseHistory.objects.filter(
                 Q(course=course) & (Q(role="I") | Q(role="T") & Q(user=request.user))
             )
@@ -99,6 +111,7 @@ class IsInstructorOrTAOrReadOnly(permissions.BasePermission):
         1. complete permissions to instructor/ta
         2. list/retrieve permissions to students
         3. list/retrieve permissions to anonymous users
+
     Applicable for: Course
     """
 
@@ -134,6 +147,7 @@ class UserPermission(permissions.BasePermission):
         1. create permission to anonymous users
         2. list/retrieve permission to authenticated users
         3. update/partial_update/destroy to authenticated owners only
+
     Applicable for: User
     """
 
@@ -161,3 +175,41 @@ class UserPermission(permissions.BasePermission):
         if request.user.is_authenticated and obj.email == request.user.email:
             return True
         return False
+
+
+class IsInstructorOrTAOrStudent(permissions.BasePermission):
+    """
+    Allows:
+        1. complete permissions to instructor/ta/students
+
+    Applicable for: VideoHistory, SingleCorrectQuestionHistory,
+                    MultipleCorrectQuestionHistory, FixedAnswerQuestionHistory,
+                    DescriptiveQuestionhistory, CourseHistory, Crib, CribReply,
+    """
+
+    def has_permission(self, request, view):
+        """
+        Applicable at model level (list, create, retrieve, update,
+                                   partial_update, destroy)
+        """
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Applicable at model instance level (retrieve, update, partial_update, destroy)
+        """
+        if request.user.is_authenticated:
+            if request.method in permissions.SAFE_METHODS:
+                return True
+
+            # find user using obj instance
+            if type(obj) == Crib:
+                user = obj.created_by
+            else:
+                user = obj.user
+
+            if user.email == request.user.email:
+                return True
+            return False

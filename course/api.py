c381f7ac760c26859206a1106b2a0f59156cc903
Poornima <sridhar.poornima18@gmail.com>
2021-04-09 20:22:01 +0530

    page api

--- course/api.py
+++ course/api.py
@@ -1,10 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from utils.drf_utils import IsInstructorOrTA, IsInstructorOrTAOrReadOnly
 
-from .models import Chapter, Course, CourseHistory
-from .serializers import ChapterSerializer, CourseHistorySerializer, CourseSerializer
+from .models import Chapter, Course, CourseHistory, Page
+from .serializers import (
+    ChapterSerializer,
+    CourseHistorySerializer,
+    CourseSerializer,
+    PageSerializer,
+)
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -24,6 +31,33 @@ class CourseViewSet(viewsets.ModelViewSet):
         "course_type",
     )
     ordering_fields = ("id",)
+
+
+class PageViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+    permission_classes = (IsInstructorOrTA,)
+    pagination_class = None
+
+    @action(detail=True, methods=["POST"])
+    def add_page(self, request, pk=None):
+        """Add a page to a course with primary key as pk"""
+        try:
+            Course.objects.get(id=pk)
+        except Course.DoesNotExist:
+            data = {"error": "Course with id: {} does not exist".format(pk)}
+            return Response(data, status.HTTP_400_BAD_REQUEST)
+        serializer = PageSerializer(data=request.data)
+        if serializer.is_valid():
+            page = serializer.save()
+            return Response(PageSerializer(page).data)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 class CourseHistoryViewSet(viewsets.ModelViewSet):

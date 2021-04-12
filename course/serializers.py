c5f4570f72bd535cd15a6dad0e2047975eea9edb
Poornima <sridhar.poornima18@gmail.com>
2021-04-12 17:51:08 +0530

    course crud related api added

--- course/serializers.py
+++ course/serializers.py
@@ -20,6 +20,32 @@ class CourseSerializer(serializers.ModelSerializer):
         DiscussionForum.objects.create(course=course, **df_settings_data)
         return course
 
+    def update(self, instance, validated_data):
+        df_settings_data = validated_data.pop("df_settings", {})
+        # HTML checkboxes (For DRF Browsable API) do not send any value,
+        # but should be treated as `False` by BooleanField
+        # df_settings_data_keys = df_settings_data.keys() if df_settings_data else []
+        # df_boolean_fields = ["anonymous_to_instructor", "send_email_to_all"]
+        # for df_boolean_field in df_boolean_fields:
+        #     if df_boolean_field not in df_settings_data_keys:
+        #         df_settings_data[df_boolean_field]=False
+        # validated_data_keys = validated_data.keys() if validated_data else []
+        # course_boolean_fields = ["is_published"]
+        # for course_boolean_field in course_boolean_fields:
+        #     if course_boolean_field not in validated_data_keys:
+        #         validated_data[course_boolean_field]=False
+        # ends here
+        # info = model_meta.get_field_info(instance)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        discussion_forum = DiscussionForum.objects.get(course=instance)
+        if df_settings_data:
+            for attr, value in df_settings_data.items():
+                setattr(discussion_forum, attr, value)
+            discussion_forum.save()
+        return instance
+
 
 class CourseHistorySerializer(serializers.ModelSerializer):
     class Meta:

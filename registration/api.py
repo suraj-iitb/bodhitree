22e05fb4b1597819c79103ac40444e45b62d99b6
Suraj Kumar <suraj.iitb.in@gmail.com>
2021-05-02 09:40:56 +0530

    registration

--- registration/api.py
+++ registration/api.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.db import IntegrityError
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -9,7 +12,12 @@ from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+logger = logging.getLogger(__name__)
+
+
+class UserViewSet(viewsets.GenericViewSet):
+    """Viewset for User."""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (UserPermission,)
@@ -18,16 +26,36 @@ class UserViewSet(viewsets.ModelViewSet):
         "full_name",
         "is_active",
     )
-    search_fields = (
-        "email",
-        "full_name",
-    )
+    search_fields = ("email", "full_name", "is_active")
     ordering_fields = ("id",)
 
-    @action(
-        methods=["post"],
-        detail=False,
-    )
+    @action(detail=False, methods=["POST"])
+    def create_user(self, request):
+        """Adds a user.
+
+        Args:
+            request (Request): DRF `Request` object
+
+        Returns:
+            `Response` with the created user data and status HTTP_201_CREATED.
+
+        Raises:
+            HTTP_400_BAD_REQUEST: Raised due to serialization errors
+            HTTP_403_FORBIDDEN: Raised by `IntegrityError` of the database
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except IntegrityError as e:
+                logger.exception(e)
+                return Response(e, status=status.HTTP_403_FORBIDDEN)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        errors = serializer.errors
+        logger.error(errors)
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["POST"])
     def logout(self, request, pk=None):
         try:
             refresh_token = request.data["refresh_token"]
@@ -35,4 +63,5 @@ class UserViewSet(viewsets.ModelViewSet):
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
+            logger.exception(e)
             return Response(e, status=status.HTTP_400_BAD_REQUEST)
--- registration/tests.py
+++ registration/tests.py
@@ -1,111 +1,19 @@
-from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from registration.models import User
-
 
 class UserViewSetTests(APITestCase):
-    fixtures = [
-        "users.test.yaml",
-    ]
-
-    def login(self, email, password):
-        self.client.login(email=email, password=password)
-
-    def logout(self):
-        self.client.logout()
-
-    @classmethod
-    def setUpTestData(cls):
-        """
-        Set up data for the whole TestCase.
-        """
-        cls.ins_cred = {"email": "instructor@bodhitree.com", "password": "instructor"}
-        cls.ta_cred = {"email": "ta@bodhitree.com", "password": "ta"}
-        cls.stu_cred = {"email": "student@bodhitree.com", "password": "student"}
-
-    # def test_get_users(self):
-    #     """
-    #     Ensure we can get all User objects.
-    #     """
-    #     self.login(**self.ins_cred)
-    #     url = reverse("registration:user-list")
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     length = User.objects.count()
-    #     self.assertEqual(len(response.data["results"]), length)
-    #     self.logout()
-
-    def test_get_user(self):
-        """
-        Ensure we can get one User object.
-        """
-        self.login(**self.ins_cred)
-        url = reverse("registration:user-detail", kwargs={"pk": 1})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["id"], 1)
-        self.logout()
+    """Test for UserViewSet."""
 
     def test_create_user(self):
-        """
-        Ensure we can create a new User object.
-        """
-        data = {"email": "test3@test.com", "password": "Test@1003"}
-        url = reverse("registration:user-list")
+        """Test to check: list all section videos."""
+        data = {
+            "email": "test@test.com",
+            "password": "Test@1001",
+        }
+        url = reverse("registration:user-create-user")
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        return_data = response.data
-        for k in ["id", "full_name", "is_active", "date_joined", "last_login"]:
-            return_data.pop(k)
-        data.pop("password")
-        self.assertEqual(return_data, data)
-
-    def test_update_user(self):
-        """
-        Ensure we can update a User object.
-        """
-        self.client.login(**self.ins_cred)
-        data = {"email": "test4@test.com", "password": "Test@1004"}
-        url = reverse(("registration:user-detail"), kwargs={"pk": 1})
-        response = self.client.put(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        return_data = response.data
-        for k in ["id", "full_name", "is_active", "date_joined", "last_login"]:
-            return_data.pop(k)
-        data.pop("password")
-        self.assertEqual(return_data, data)
-        self.logout()
-
-    def test_partial_update_user(self):
-        """
-        Ensure we can partial update a User object.
-        """
-        self.client.login(**self.ins_cred)
-        data = {
-            "email": "test6@test.com",
-        }
-        url = reverse(("registration:user-detail"), kwargs={"pk": 1})
-        response = self.client.patch(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        return_data = response.data
-        for k in ["id", "full_name", "is_active", "date_joined", "last_login"]:
-            return_data.pop(k)
-        self.assertEqual(return_data, data)
-        self.logout()
-
-    def test_delete_user(self):
-        """
-        Ensure we can delete a User object.
-        """
-        user = User.objects.create_user("test7@test.com", "Test@1007")
-        user.save()
-        self.client.login(email="test7@test.com", password="Test@1007")
-        url = reverse(("registration:user-detail"), kwargs={"pk": user.id})
-        response = self.client.delete(url)
-        try:
-            User.objects.get(id=user.id)
-        except ObjectDoesNotExist:
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        if response.status_code == status.HTTP_201_CREATED:
+            self.assertEqual(response.data["email"], data["email"])

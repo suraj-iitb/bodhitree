563b55e1ecad586c2767038f85aebccd82b08363
Poornima <sridhar.poornima18@gmail.com>
2021-05-04 11:56:00 +0545

    test_get_course_testing_done

--- utils/tests/tests_permissions.py
+++ utils/tests/tests_permissions.py
@@ -1,8 +1,36 @@
 from django.contrib.auth.models import AnonymousUser
 from rest_framework.test import APIRequestFactory, APITestCase
 
-from course.models import Chapter, Course, CourseHistory
+from course.models import (
+    Announcement,
+    Chapter,
+    Course,
+    CourseHistory,
+    Notification,
+    Page,
+    Schedule,
+    Section,
+)
+from discussion_forum.models import DiscussionForum
+from document.models import Document
+from email_notices.models import Email
+from programming_assignments.models import (
+    AdvancedProgrammingAssignment,
+    AssignmentSection,
+    Exam,
+    SimpleProgrammingAssignment,
+    Testcase,
+)
+from quiz.models import (
+    DescriptiveQuestion,
+    FixedAnswerQuestion,
+    MultipleCorrectQuestion,
+    QuestionModule,
+    Quiz,
+    SingleCorrectQuestion,
+)
 from registration.models import User
+from subjective_assignments.models import SubjectiveAssignment
 from utils.permissions import (
     IsAdmin,
     IsInstructorOrTA,
@@ -11,6 +39,7 @@ from utils.permissions import (
     IsOwner,
     UserPermission,
 )
+from video.models import QuizMarker, SectionMarker, Video
 
 
 class PermissionHelperMixin:
@@ -48,6 +77,31 @@ class IsInstructorOrTATest(APITestCase, PermissionHelperMixin):
         "courses.test.yaml",
         "coursehistories.test.yaml",
         "chapters.test.yaml",
+        "sections.test.yaml",
+        "videos.test.yaml",
+        "documents.test.yaml",
+        "quiz.test.yaml",
+        "question.test.yaml",
+        "questionmodule.test.yaml",
+        "sectionmarker.test.yaml",
+        "quizmarker.test.yaml",
+        "singlecorrectquestion.test.yaml",
+        "multiplecorrectquestion.test.yaml",
+        "fixedanswerquestion.test.yaml",
+        "descriptivequestion.test.yaml",
+        "schedule.test.yaml",
+        "pages.test.yaml",
+        "announcement.test.yaml",
+        "discussionforum.test.yaml",
+        "assignment.test.yaml",
+        "simpleprogrammingassignment.test.yaml",
+        "advancedprogrammingassignment.test.yaml",
+        "assignmentsection.test.yaml",
+        "testcase.test.yaml",
+        "exam.test.yaml",
+        "subjectiveassignment.test.yaml",
+        "notification.test.yaml",
+        "email.test.yaml",
     ]
 
     @classmethod
@@ -61,6 +115,31 @@ class IsInstructorOrTATest(APITestCase, PermissionHelperMixin):
         cls.student = User.objects.get(id=3)
 
         cls.chapter = Chapter.objects.get(id=1)
+        cls.section = Section.objects.get(id=1)
+        cls.video = Video.objects.get(id=1)
+        cls.document = Document.objects.get(id=1)
+        cls.quiz = Quiz.objects.get(id=1)
+        cls.questionmodule = QuestionModule.objects.get(id=1)
+        cls.sectionmarker = SectionMarker.objects.get(id=1)
+        cls.quizmarker = QuizMarker.objects.get(id=1)
+        cls.singlecorrectquestion = SingleCorrectQuestion.objects.get(id=1)
+        cls.multiplecorrectquestion = MultipleCorrectQuestion.objects.get(id=3)
+        cls.fixedanswerquestion = FixedAnswerQuestion.objects.get(id=5)
+        cls.descriptivequestion = DescriptiveQuestion.objects.get(id=7)
+        cls.schedule = Schedule.objects.get(id=1)
+        cls.page = Page.objects.get(id=1)
+        cls.announcement = Announcement.objects.get(id=1)
+        cls.discussionforum = DiscussionForum.objects.get(id=1)
+        cls.simpleprogrammingassignment = SimpleProgrammingAssignment.objects.get(id=1)
+        cls.advancedprogrammingassignment = AdvancedProgrammingAssignment.objects.get(
+            id=1
+        )
+        cls.assignmentsection = AssignmentSection.objects.get(id=1)
+        cls.testcase = Testcase.objects.get(id=1)
+        cls.exam = Exam.objects.get(id=1)
+        cls.subjectiveassignment = SubjectiveAssignment.objects.get(id=1)
+        cls.notification = Notification.objects.get(id=1)
+        cls.email = Email.objects.get(id=1)
 
     def setUp(self):
         """Allows the creation of initial data at the method level."""
@@ -107,6 +186,170 @@ class IsInstructorOrTATest(APITestCase, PermissionHelperMixin):
         # TODO: For all possible objects do assertEqual
         actual_course = self.permission_class._get_course_from_object(self.chapter)
         expected_course = self.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.section)
+        expected_course = self.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.video)
+        try:
+            expected_course = self.video.chapter.course
+        except Exception:
+            expected_course = self.video.section.chapter.course.id
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.document)
+        try:
+            expected_course = self.document.chapter.course
+        except Exception:
+            expected_course = self.document.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.quiz)
+        try:
+            expected_course = self.quiz.chapter.course
+        except Exception:
+            expected_course = self.quiz.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.questionmodule
+        )
+        try:
+            expected_course = self.questionmodule.quiz.chapter.course
+        except Exception:
+            expected_course = self.questionmodule.quiz.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.sectionmarker
+        )
+        try:
+            expected_course = self.sectionmarker.video.chapter.course
+        except Exception:
+            expected_course = self.sectionmarker.video.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.quizmarker)
+        try:
+            expected_course = self.quizmarker.video.chapter.course
+        except Exception:
+            expected_course = self.quizmarker.video.section.chapter.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.singlecorrectquestion
+        )
+        try:
+            expected_course = (
+                self.singlecorrectquestion.question_module.quiz.chapter.course
+            )
+        except Exception:
+            expected_course = (
+                self.singlecorrectquestion.question_module.quiz.section.chapter.course
+            )
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.multiplecorrectquestion
+        )
+        try:
+            expected_course = (
+                self.multiplecorrectquestion.question_module.quiz.chapter.course
+            )
+        except Exception:
+            expected_course = (
+                self.multiplecorrectquestion.question_module.quiz.section.chapter.course
+            )
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.fixedanswerquestion
+        )
+        try:
+            expected_course = (
+                self.fixedanswerquestion.question_module.quiz.chapter.course
+            )
+        except Exception:
+            expected_course = (
+                self.fixedanswerquestion.question_module.quiz.section.chapter.course
+            )
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.descriptivequestion
+        )
+        try:
+            expected_course = (
+                self.descriptivequestion.question_module.quiz.chapter.course
+            )
+        except Exception:
+            expected_course = (
+                self.descriptivequestion.question_module.quiz.section.chapter.course
+            )
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.schedule)
+        expected_course = self.schedule.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.page)
+        expected_course = self.page.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.announcement)
+        expected_course = self.announcement.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.discussionforum
+        )
+        expected_course = self.discussionforum.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.simpleprogrammingassignment
+        )
+        expected_course = self.simpleprogrammingassignment.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.advancedprogrammingassignment
+        )
+        advancedprogrammingassignment = self.advancedprogrammingassignment
+        simpleprogrammingassignment = (
+            advancedprogrammingassignment.simple_programming_assignment
+        )
+        expected_course = simpleprogrammingassignment.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.assignmentsection
+        )
+        expected_course = self.assignmentsection.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.testcase)
+        expected_course = self.testcase.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.exam)
+        expected_course = self.exam.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(
+            self.subjectiveassignment
+        )
+        expected_course = self.subjectiveassignment.assignment.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.notification)
+        expected_course = self.notification.course
+        self.assertEqual(actual_course, expected_course)
+
+        actual_course = self.permission_class._get_course_from_object(self.email)
+        expected_course = self.email.course
         self.assertEqual(actual_course, expected_course)
 
 

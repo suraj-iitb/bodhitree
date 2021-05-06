872fa48812d71e77de99fba000f7f50c61620047
Suraj Kumar <suraj.iitb.in@gmail.com>
2021-05-06 19:55:30 +0530

    mixins

--- utils/mixins.py
+++ utils/mixins.py
@@ -10,7 +10,9 @@ from utils.utils import check_course_registration, check_is_instructor_or_ta
 logger = logging.getLogger(__name__)
 
 
-class IsRegisteredMixins:
+class IsRegisteredMixin:
+    """Mixin for course registration."""
+
     def _is_registered(self, course_id, user):
         """Checks if the user is registered in the given course.
 
@@ -57,80 +59,8 @@ class IsRegisteredMixins:
         logger.error(error)
         return Response(error, status.HTTP_403_FORBIDDEN)
 
-    def _is_registered_using_chapter_id(self, chapter_id, user):
-        """Checks if the user is registered in the given course.
 
-        Args:
-            chapter_id (int): chapter id
-            user (User): `User` model object
-
-        Returns:
-            A 2-valued tuple where
-                first_value: boolean representing whether the user is registered
-                    in the course
-                second_value: course id
-
-        Raises:
-            HTTP_404_NOT_FOUND: Raised if:
-                1. The chapter does not exist
-                2. The user is not registered in the course
-        """
-        try:
-            chapter = Chapter.objects.select_related("course").get(id=chapter_id)
-        except Chapter.DoesNotExist as e:
-            logger.exception(e)
-            return (Response(e, status.HTTP_404_NOT_FOUND), None)
-
-        course_id = chapter.course.id
-        if not check_course_registration(course_id, user):
-            data = {
-                "error": "User: {} is not registered in the course with id: {}.".format(
-                    user, course_id
-                ),
-            }
-            logger.warning(data["error"])
-            return (Response(data, status.HTTP_404_NOT_FOUND), None)
-        return (True, course_id)
-
-    def _is_registered_using_section_id(self, section_id, user):
-        """Checks if the user is registered in the given course.
-
-        Args:
-            section_id (int): section id
-            user (User): `User` model object
-
-        Returns:
-            A 2-valued tuple where
-                first_value: boolean representing whether the user is registered
-                    in the course
-                second_value: course id
-
-        Raises:
-            HTTP_404_NOT_FOUND: Raised if:
-                1. The chapter does not exist
-                2. The user is not registered in the course
-        """
-        try:
-            section = Section.objects.select_related("chapter__course").get(
-                id=section_id
-            )
-        except Section.DoesNotExist as e:
-            logger.exception(e)
-            return (Response(e, status.HTTP_404_NOT_FOUND), None)
-
-        course_id = section.chapter.course.id
-        if not check_course_registration(course_id, user):
-            data = {
-                "error": "User: {} is not registered in the course with id: {}.".format(
-                    user, course_id
-                ),
-            }
-            logger.warning(data["error"])
-            return (Response(data, status.HTTP_404_NOT_FOUND), None)
-        return (True, course_id)
-
-
-class ContentMixin(IsRegisteredMixins):
+class ContentMixin(IsRegisteredMixin):
     """Mixin for content (`Video` or `Document`)."""
 
     def create_content(self, request):

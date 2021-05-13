c7b03d7e297f5c252cb5aaa3b0735d56ee0aa2a0
Suraj Kumar <suraj.iitb.in@gmail.com>
2021-05-13 10:18:57 +0530

    test for email

---
+++ utils/tests/tests_mails.py
@@ -0,0 +1,22 @@
+from django.core import mail
+from django.test import TestCase
+
+from utils.mails import send_mail
+
+
+class EmailTest(TestCase):
+    """Email Test"""
+
+    def test_send_email(self):
+        send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to1@example.com", "to2@example.com"],
+        )
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Subject here")
+        self.assertEqual(mail.outbox[0].body, "Here is the message.")
+        self.assertEqual(mail.outbox[0].from_email, "from@example.com")
+        self.assertEqual(mail.outbox[0].to, ["to1@example.com", "to2@example.com"])
